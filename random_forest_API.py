3da75176d250283726daf6053f944b006b14001f
5Y5TEM <meltem@subasioglu.de>
2022-02-16 22:47:28 +0100

    added RF model and API

---
+++ random_forest_API.py
@@ -0,0 +1,45 @@
+# Created by MeltemSubasioglu at 2/16/2022
+
+import pickle
+from flask import Flask, request
+import numpy as np
+import pandas as pd
+import os
+dirname = os.path.dirname(__file__)
+
+# Load model in read binary mode
+with open(dirname+ r"/rf.pkl", 'rb') as model_file:
+    model = pickle.load(model_file)
+
+
+# Start the flask app
+app = Flask(__name__)
+
+# GET Request with URI Params
+@app.route("/predict")
+def get_prediction():
+    s_length = request.args.get("s_length")
+    s_width = request.args.get("s_width")
+    p_length = request.args.get("p_length")
+    p_width = request.args.get("p_width")
+
+    # Parse the features as list of lists
+    prediction = model.predict(np.array([[s_length, s_width, p_length, p_width]]))
+
+    return str(prediction)
+
+
+# POST Request with File Input
+@app.route("/predict_file", methods=["POST"])
+def get_prediction_file():
+    # Reading filed in Flask with request.files
+    input_data = pd.read_csv(request.files.get("input_file"), header=None)
+
+    # Parse the features as list of lists
+    prediction = model.predict(np.array(input_data))
+    return str(list(prediction))
+
+
+
+if __name__ == "__main__":
+    app.run()
---
+++ random_forest_IRIS.py
@@ -0,0 +1,37 @@
+# Created by MeltemSubasioglu at 2/16/2022
+# Source udemy vivekkalyanarangan
+
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.datasets import load_iris
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import accuracy_score
+import pandas as pd
+import pickle #saving models as binary files
+import os
+dirname = os.path.dirname(__file__)
+
+
+# Loading the dataset
+iris = load_iris()
+X = iris.data
+y = iris.target
+
+# Splitting the data
+X_train, X_test, y_train, y_test = train_test_split(X,y, random_state=42, test_size=0.5)
+
+# Building the model
+model = RandomForestClassifier(n_estimators = 10)
+
+# Training the classifier
+model.fit(X_train, y_train)
+
+# Predictions
+predicted = model.predict(X_test)
+
+# Check accuracy
+print(accuracy_score(predicted, y_test))
+# path = (dirname+ r"/rf.pkl")
+
+# Saving the model
+with open(dirname+ r"/rf.pkl", 'wb') as model_pkl:
+    pickle.dump(model, model_pkl)
